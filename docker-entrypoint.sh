cfd838132e762e636954c55eac77021155646ac5
Kauan Carvalho Calasans <55901289+KauanCarvalho@users.noreply.github.com>
2024-02-14 18:52:00 -0300

    Adjust query to return transactions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+until nc -z -v -w20 database 5432; do
+  >&2 echo "PostgreSql is unavailable - sleeping"
+	sleep 2;
+done
+
 if [ "$1" = 'release' ]; then
   exec /app/bin/backend_fight start
 elif [ "$1" = 'migrate_and_release' ]; then
